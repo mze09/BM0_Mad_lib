bf33e05e94951d1212c2b609720ceb588a570fad
mze09 <113617375+mze09@users.noreply.github.com>
2025-11-24 10:52:39 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,4 +1,16 @@
-print("enter an adjective:")
+print("enter an adjective: ")
 adjective = input()
 
-print(f"you wrote {adjective}")
+print("enter another adjective: ")
+adjective2 = input()
+
+print("enter a color: ")
+color = input()
+
+print("enter a noun: ")
+noun = input()
+
+print("enter another noun: ")
+noun2 = input()
+
+print(f"the {adjective} {color} {noun} jumps over the {adjective2} {noun2}.")
