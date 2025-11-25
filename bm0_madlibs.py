392b08fd72c0865a620405b0d215eb8697ed1991
mze09 <113617375+mze09@users.noreply.github.com>
2025-11-25 11:54:09 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,16 +1,43 @@
-print("enter an adjective: ")
-adjective = input()
+import random
+import time
+story = random.randint(1, 2)
 
-print("enter another adjective: ")
-adjective2 = input()
+if(story == 1):
+	print("enter an adjective: ")
+	adjective = input()
 
-print("enter a color: ")
-color = input()
+	print("enter another adjective: ")
+	adjective2 = input()
 
-print("enter a noun: ")
-noun = input()
+	print("enter a color: ")
+	color = input()
 
-print("enter another noun: ")
-noun2 = input()
+	print("enter the name of an animal: ")
+	noun = input()
 
-print(f"the {adjective} {color} {noun} jumps over the {adjective2} {noun2}.")
+	print("enter the name of another animal: ")
+	noun2 = input()
+
+	print(f"the {adjective} {color} {noun} jumps over the {adjective2} {noun2}.")
+else:
+	print("enter the name of a language: ")
+	language = input()
+
+	print("enter the name of a game:")
+	game = input()
+
+	print("enter the name of a food:")
+	food = input()
+
+	print("enter an object: ")
+	objectinput = input()
+
+	print(f"apolgy for bad {language}")
+	time.sleep(1)
+	print(f"where were u wen {game} die")
+	time.sleep(1)
+	print(f"i was at house eating {food} when {objectinput} ring")
+	time.sleep(1)
+	print(f"{game} is kil")
+	time.sleep(1)
+	print(f"no")
